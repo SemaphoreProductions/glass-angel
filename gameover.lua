ba0e5f4d515e5455c0eb0e696c5ca5fdf2e620f7
Jane Lewis <inquisitivepenguin@protonmail.com>
2022-12-01 10:20:37 -0800

    Fix missing assets

---
+++ gameover.lua
@@ -0,0 +1,24 @@
+local gameover = ...
+
+local audio = require 'audio'
+
+local Menu = require 'menu'
+
+gameover.scene = am.group():tag"gameover" ^ {
+    am.rect(-screenEdge.x, screenEdge.y * 2, screenEdge.x, -screenEdge.y, vec4(0, 0, 0, 0.5)),
+    large_text_node("Game Over"),
+    button("continue", 2, function()
+        Life = 5
+        for _, child in globalWindow.scene:child_pairs() do
+            child.paused = false
+        end
+        globalWindow.scene:remove("gameover")
+    end),
+    button("return to menu", 3, function()
+        Score = 0
+        globalWindow.scene = Menu:new()
+    end)
+}
+
+gameover.scene:action("music", am.play(audio.stageclear, true, 0.25))
+
