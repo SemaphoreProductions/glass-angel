780b0ee99e7867b2afbadbf5cbdd5ac24c026015
actually-reb <klumpy22@gmail.com>
2022-12-13 18:57:58 -0800

    cleaned up character node class

--- reader.lua
+++ reader.lua
@@ -3,42 +3,43 @@ local Reader = ...
 local bg = require 'bg'
 
 
-function Reader.new(script)
+function Reader:new(script)
     local reader = {
         script = script,
         item = 1,
         scene = 1,
     }
-
-    function reader:init_stage(scene)
-        local music = reader.script[reader.item].bgmusic
-        local background = reader.script[reader.item].bg
-        local loop = reader.script[reader.item].musicloop ~= false
-        if music ~= nil then
-            scene.track = am.track(music, loop, 1, VOLUME)
-            scene:action("music", am.play(scene.track))
-        end
-        if background ~= nil then
-            bg.set_texture(scene, background)
-        end
-    end
-    
-    function reader:update(scene)
-        if scene("continue") ~= nil then
-            local current_scene = script[reader.item].scenes[reader.scene]
-            scene:remove("continue")
-            scene:action("script", current_scene)
-            if reader.scene == 1 then
-                reader:init_stage(scene)
-            end
-            if #reader.script[reader.item].scenes < reader.scene + 1 then
-                reader.item = reader.item + 1 
-                reader.scene = 1
-            else
-                reader.scene = reader.scene + 1
-            end
-        end
-    end
-
+    setmetatable(reader, self)
+    self.__index = self
     return reader
+end
+
+function Reader:init_stage(scene)
+    local music = self.script[self.item].bgmusic
+    local background = self.script[self.item].bg
+    local loop = self.script[self.item].musicloop ~= false
+    if music ~= nil then
+        scene.track = am.track(music, loop, 1, VOLUME)
+        scene:action("music", am.play(scene.track))
+    end
+    if background ~= nil then
+        bg.set_texture(scene, background)
+    end
+end
+
+function Reader:update(scene)
+    if scene("continue") ~= nil then
+        local current_scene = self.script[self.item].scenes[self.scene]
+        scene:remove("continue")
+        scene:action("script", current_scene)
+        if self.scene == 1 then
+            self:init_stage(scene)
+        end
+        if #self.script[self.item].scenes < self.scene + 1 then
+            self.item = self.item + 1 
+            self.scene = 1
+        else
+            self.scene = self.scene + 1
+        end
+    end
 end
